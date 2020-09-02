6a39fe2a973fb3d78a41fe3c638fc7330a7e5348
Hamid <mail.hamid.hoseini@gmail.com>
2020-09-01 17:30:09 -0700

    Login Layout

---
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by Hamid Hoseini on 8/31/20.
+//  Copyright © 2020 Hamid Hoseini. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .link
+    }
+    
+}
---
+++ Messenger/Resources/extensions.swift
@@ -0,0 +1,36 @@
+//
+//  extensions.swift
+//  Messenger
+//
+//  Created by Hamid Hoseini on 9/1/20.
+//  Copyright © 2020 Hamid Hoseini. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
